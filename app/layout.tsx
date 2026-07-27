cb628f1ff82a73d2739687a4d5647b589efce8ff
Aniket <aniketnaik2004@gmailcom>
2026-07-27 13:18:30 +0530

    feat: add Next.js agent rules and integrate Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils";
+
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
@@ -23,8 +25,12 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
+
     </html>
   )
 }
